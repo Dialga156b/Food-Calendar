0fb7bff9c89d770c48034f4b0cfcbf1a36550bf5
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-04-26 21:50:36 -0400

    make the todays recipe thing actually work

--- web/JS/todaysRecipe.js
+++ web/JS/todaysRecipe.js
@@ -11,15 +11,15 @@ function loadRecipe(ids) {
       image: recipeImgLink,
       id: recipeID,
     */
-  console.log(ids);
-
   recipes = JSON.parse(localStorage.getItem("recipes")) || {};
   document.querySelectorAll("#tr-active").forEach((element) => {
     element.remove();
   });
-  if (ids.length == 0) {
-    console.log("no items");
+  const headerLbl = document.getElementById("tr-header");
+  if (!ids) {
+    headerLbl.textContent = "Nothing scheduled for today";
   } else {
+    headerLbl.textContent = "Today";
     const tab = document.getElementById("todaysRecipeTab");
     const rTemplate = document.getElementById("tr-template");
     for (let index = 0; index < ids.length; index++) {
@@ -31,24 +31,39 @@ function loadRecipe(ids) {
       thisRecipe.classList = "tr-active";
       const rName = thisRecipe.querySelector("#tr-name");
       const rDesc = thisRecipe.querySelector("#tr-desc");
+      const img = thisRecipe.querySelector("#tr-img");
       const cal = thisRecipe.querySelector("#f-cal");
       const prep = thisRecipe.querySelector("#f-prep");
       const cook = thisRecipe.querySelector("#f-cook");
       const servings = thisRecipe.querySelector("#f-serv");
       const ingredient = thisRecipe.querySelector("#ingredient");
       const step = thisRecipe.querySelector("#step");
+
       const stepNum = step.children[0];
       const stepName = step.children[1];
 
       rName.textContent = recipe.recipe_name;
       rDesc.textContent = recipe.desc;
       cal.textContent = recipe.calories;
-      prep.textContent = recipe.prep_time_minutes;
-      cook.textContent = recipe.cook_time_minutes;
+      prep.textContent = `${recipe.prep_time_minutes}m`;
+      cook.textContent = `${recipe.cook_time_minutes}m`;
       servings.textContent = recipe.servings;
+      img.src = recipe.image;
 
       for (let index = 0; index < recipe.ingredients.length; index++) {
-        const thisIngredient = ingredient.cloneNode(true);
+        const thisIngredientEl = ingredient.cloneNode(true);
+        thisIngredientEl.style.display = "list-item";
+        thisRecipe.querySelector("#ingredients").appendChild(thisIngredientEl);
+        const thisIngredient = recipe.ingredients[index];
+        thisIngredientEl.innerHTML = `${thisIngredient.amount} ${thisIngredient.name}`;
+      }
+      for (let index = 0; index < recipe.instructions.length; index++) {
+        const thisStepEl = step.cloneNode(true);
+        thisStepEl.style.display = "flex";
+        thisRecipe.querySelector("#instructions").appendChild(thisStepEl);
+        const thisStep = recipe.instructions[index];
+        thisStepEl.children[0].textContent = index + 1;
+        thisStepEl.children[1].textContent = thisStep.description;
       }
     }
   }
