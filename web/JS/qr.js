b07878f180778ae5094fd6c87234800b74e10919
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-05-02 16:24:54 -0400

    hi

--- web/JS/qr.js
+++ web/JS/qr.js
@@ -4,8 +4,7 @@ async function showQRCode(mode, ingredients) {
   while (QRContainer.firstChild) {
     QRContainer.removeChild(QRContainer.firstChild);
   }
-  console.log(ingredients);
-  const link = `http://127.0.0.1:5500/Food-Calendar/web/mobileShopping.html?data=${ingredients}`;
+  const link = `https://food-calendar-eight.vercel.app/Food-Calendar/web/mobileShopping.html?data=${ingredients}`;
   const qr = await makeQrCode(link);
   console.log(qr);
   if (mode) {
@@ -19,11 +18,14 @@ async function showQRCode(mode, ingredients) {
   }
 }
 async function makeQrCode(link) {
-  const res = await fetch("/api/bitly", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ long_url: "https://example.com/somepage" }),
-  });
+  const res = await fetch(
+    "https://food-calendar-eight.vercel.app/api/create-qr",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ long_url: link }),
+    }
+  );
   const data = await res.json();
   console.log("Short URL:", data.bitlink_url);
   console.log("QR Code Image:", data.qr_code_url);
