2271735503aeb331575de3c137e79ac1b0d029d6
BryceHeitzmann <109158364+Dialga156b@users.noreply.github.com>
2025-07-15 22:30:37 -0400

    yay

--- web/JS/recipeSettings.js
+++ web/JS/recipeSettings.js
@@ -11,10 +11,13 @@ async function showRS(mode, id = false) {
       const recipe = recipes[id];
       const site = recipe.site;
       const siteBtn = document.getElementById("btn-web");
+      const addSiteBtn = document.getElementById("btn-add-web");
       if (siteBtn) {
         if (site) {
+          addSiteBtn.style.display = "none";
           siteBtn.style.display = "block";
         } else {
+          addSiteBtn.style.display = "block";
           siteBtn.style.display = "none";
         }
       }
@@ -49,5 +52,23 @@ function website() {
   }
 }
 
+function addRecipeWebsite() {
+  const recipeId = window.CR_ID;
+  const reply = prompt(
+    "Insert website link (this won't change the content of the recipe):"
+  );
+
+  if (reply) {
+    const recipes = JSON.parse(localStorage.getItem("recipes") || "{}");
+    if (recipes.hasOwnProperty(recipeId)) {
+      recipes[recipeId].site = reply;
+      localStorage.setItem("recipes", JSON.stringify(recipes));
+    } else {
+      alert("dude.");
+    }
+  }
+  showRS(false);
+}
 window.showRS = showRS;
 window.website = website;
+window.addRecipeWebsite = addRecipeWebsite;
